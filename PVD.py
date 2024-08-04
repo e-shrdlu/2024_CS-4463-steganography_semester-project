4043e0b1158e327ad3877262a16d23be5e8ec17a
shrdlu <broyles.nat@gmail.com>
2024-08-04 17:07:23 -0600

    cleaned up code, added functionality, added description / instructions to readme

--- PVD.py
+++ PVD.py
@@ -10,15 +10,14 @@ import argparse
 import os
 import time
 import math
-import struct
 from PIL import Image
 
-# global vars / settings #
-##########################
+# global vars / settings / default options #
+############################################
 
 # placeholders for now, will update generator function to do more later
 pixel_pair_mode = "horizontal"
-pixel_iteration_mode = "standard"
+pixel_iteration_mode = "standard" # no other methods implemented yet
 color_mode = "grayscale" # "grayscale" or "color" - defaults to "grayscale" but can be set in cmdline args
 diff_ranges = [0,8,16,32,64,128,256] # hold indices of where ranges begin/end. First/Last elements should always be 0 and 256
 num_bits_for_size=32 # this many bits will be used in the beginning of the image to determine how many bytes of data should be read
@@ -42,18 +41,9 @@ class bitmap:
 
 # command line arguments #
 ##########################
+
 def init_commandline_args():
-    parser = argparse.ArgumentParser(prog="PVD", description="Pixel Value Differencing Steganography") #, epilog=textwrap.dedent("""
-        # example usage:
-        #     python3 PVD.py -c my-cover-image.bmp -m my-secret-message.txt -o my-new-image.bmp
-        #      - hide file into cover image
-        #
-        #     python3 PVD.py -c my-cover-image.bmp -n
-        #      - check capacity of my-cover-image.bmp
-        #
-        #     python3 PVD.py -e my-new-image.bmp -o my-recovered-message.txt
-        #      - extract file from stego image"""))
-        # ^ ^ ^ ^ ^ ^ ^ ^ tried to add to help text, but it wasn't working (argparse strips the newlines and it looks kinda bad
+    parser = argparse.ArgumentParser(prog="PVD", description="Pixel Value Differencing Steganography")
 
     parser.add_argument("-c", "--cover-image", help="cover image to hide message in")
     parser.add_argument("-m", "--message-file", help="file containing secret message to hide")
@@ -66,6 +56,8 @@ def init_commandline_args():
     parser.add_argument("--gray", help="convert to grayscale and hide only in luminance (defualts to --gray if unspecified)", action="store_true")
     parser.add_argument("--horizontal", help="use horizontal pixel pairs (defualts to --horizontal if unspecified)", action="store_true")
     parser.add_argument("--vertical", help="use vertical pixel pairs (defualts to --horizontal if unspecified)", action="store_true")
+    parser.add_argument("--key", help="key for randomized hiding locations")
+
     args = parser.parse_args()
     return args
 
@@ -99,14 +91,42 @@ def validate_args(args): # ensures user has given the correct options, and the n
             print("warning: output image", args.output_file, "already exists. You have 5s to cancel...")
             time.sleep(5)
 
+def apply_settings_from_args(args):
+    global color_mode
+    global pixel_pair_mode
+    global debug_mode
+    global quiet_mode
+
+    if args.verbose:
+        debug_mode = 1
+
+    if args.quiet:
+        quiet_mode = 1
+
+    if args.vertical:
+        pixel_pair_mode = "vertical"
+    if args.horizontal:
+        pixel_pair_mode = "horizontal"
+
+    if args.rgb:
+        color_mode = "color"
+    elif args.gray:
+        color_mode = "grayscale"
+
 # Image iterating generator thing #
 ###################################
 # using pythons generators will allow us to write logic for iterating through pixel-pairs
 # in different ways, allowing experimentation with different pairs (horizontal vs vertical)
 # and different paths (left->right, top->bottom vs randomly determined with symmetric key, etc)
-# stores pair as ((R,G,B), (R,G,B)) for color
-# or as (brightness, brightness) for grayscale
-def pixel_pairs(img): # takes PIL Image object as parameter
+def pixel_pairs(img, key=None): # takes PIL Image object as parameter
+    if key:
+        import random
+        random.seed(key)
+        coords = list(pixel_pairs(img, key=None))
+        random.shuffle(coords)
+        for c in coords:
+            yield c
+
     size_x, size_y = img.size
     if pixel_pair_mode == "horizontal":
         if pixel_iteration_mode == "standard":
@@ -130,12 +150,22 @@ def pixel_pairs(img): # takes PIL Image object as parameter
         print("ERROR: pixel pair mode", pixel_pair_mode, "not recognized")
         exit()
 
-def calculate_rgb_difference(pixel1, pixel2):
-    # Calculate the difference between 2 RGB pixel values, will be used for determining range to hide in
-    return (abs(pixel1[0] - pixel2[0]), abs(pixel1[1] - pixel2[1]), abs(pixel1[2] - pixel2[2]))
+# functions for calculating and applying difference #
+#####################################################
 
-def calculate_gray_difference(pixel1, pixel2):
+def get_log2(val): # ended up doing this a lot so we made a function
+    return math.floor(math.log2(val))
+
+def calculate_difference(pixel1, pixel2):
     return abs(pixel1-pixel2)
+
+def find_difference_range(pixel_pair): # this is the range the difference falls into, ie diff=5 -> [0,1,2,3,4,5,6,7]
+    diff = abs(pixel_pair[0] - pixel_pair[1])
+    for i in range(1, len(diff_ranges)):
+        if diff < diff_ranges[i]:
+            diff_range = list(range(diff_ranges[i-1], diff_ranges[i]))
+            return diff_range
+    print("ERROR: for some reason did not calculate range for diff =", diff)
 
 def get_embedding_capacity(diff):
     # embedding capacity = log2(width of range)
@@ -149,30 +179,14 @@ def get_embedding_capacity(diff):
         if diff < diff_ranges[i]: # if this is the right diff range
             return get_log2(diff_ranges[i] - diff_ranges[i-1]) # capacity floor(log2(upper - lower))
 
-def get_log2(val):
-    return math.floor(math.log2(val))
-
-def read_message_file(filepath):
-    with open(filepath, 'r') as file:
-        message = file.read()
-    # Convert message to binary string
-    return ''.join(format(ord(char), '08b') for char in message)
-
-def find_difference_range(pixel_pair):
-    diff = abs(pixel_pair[0] - pixel_pair[1])
-    for i in range(1, len(diff_ranges)):
-        if diff < diff_ranges[i]:
-            diff_range = list(range(diff_ranges[i-1], diff_ranges[i]))
-            return diff_range
-    print("ERROR: for some reason did not calculate range for diff =", diff)
-
-
-def grayscale_new_vals(pixel_pair, new_diff, diff): 
-    # from the paper
+# change pixel values to have correct difference, while minimizing and balancing change
+# formula provided from the paper
+def calculate_new_vals_from_difference(pixel_pair, new_diff, diff):
     m = new_diff - diff # m is new_difference - old_difference. I guess you could name it like "diffdiff", but I thought it'd be less confusing to just use the paper's variable
-    m = -m
+
+    # this part is weird, it was having trouble calculating it right, and would sometimes flip the signs of what to add
+    # but not all the time. So I just added a check where if the difference was wrong, do it again but flip the sign
     while True:
-        m = -m
         if debug_mode and not quiet_mode: print("[dbg] m =",m, "m/2 =", m/2, " diff =", diff)
         if diff % 2 == 1: # diff is odd
             if debug_mode and not quiet_mode: print("[dbg] diff is odd")
@@ -182,60 +196,18 @@ def grayscale_new_vals(pixel_pair, new_diff, diff):
             if debug_mode and not quiet_mode: print("[dbg] diff is even")
             new_vals = (pixel_pair[0] - math.floor(m/2), pixel_pair[1] + math.ceil(m/2))
             if debug_mode and not quiet_mode: print("[dbg]", "(",pixel_pair[0]," - (floor)",math.floor(m/2),", ",pixel_pair[1]," + (ceil)",math.ceil(m/2),")")
-        if calculate_gray_difference(new_vals[0], new_vals[1]) == new_diff:
+
+        # if difference is right, break and return. Else try again with flipped sign
+        if calculate_difference(new_vals[0], new_vals[1]) == new_diff:
             break
+        m = -m
     return new_vals
 
-def extract_data(steg_image):
-    bits = ""
-    bytes_to_read = 0 # will change later
 
-    for pixel_coords_1, pixel_coords_2 in pixel_pairs(steg_image):
-        # break when we've read all the message bits #
-        ##############################################
-        if len(bits) > num_bits_for_size and not bytes_to_read: # wait to determine msg size until we've read enough bits
-            bytes_to_read = int(bits[0:num_bits_for_size],2)
-        if bytes_to_read and len(bits) >= (8*bytes_to_read + num_bits_for_size): #
-            break
-
-        if debug_mode and not quiet_mode: print(f"[dbg] in extract_data(), color_mode is {color_mode}")
-        if color_mode == "grayscale":
-            pixel_components = [[steg_image.getpixel(pixel_coords_1),steg_image.getpixel(pixel_coords_2)]]
-        elif color_mode == "color":
-            pixel_components = [[steg_image.getpixel(pixel_coords_1)[0],steg_image.getpixel(pixel_coords_2)[0]], [steg_image.getpixel(pixel_coords_1)[1],steg_image.getpixel(pixel_coords_2)[1]], [steg_image.getpixel(pixel_coords_1)[2],steg_image.getpixel(pixel_coords_2)[2]]]
-        else:
-            print("ERROR: color mode unrecognized:", color_mode)
-            exit()
-        
-        for pixel1, pixel2 in pixel_components:
-            # find difference range for pixel pair
-            # and also check if valid pixel pair
-            #######################################
-            diff = calculate_gray_difference(pixel1, pixel2)
-            diff_range = find_difference_range((pixel1, pixel2))
-            pixel_pair_possible_vals = grayscale_new_vals((pixel1, pixel2), diff_range[-1], diff)
-            # from the paper. determine if pixelpair should be skipped:
-            if pixel_pair_possible_vals[0] < 0 or pixel_pair_possible_vals[0] > 255 or pixel_pair_possible_vals[1] < 0 or pixel_pair_possible_vals[1] > 255:
-                # invalid pair
-                if not quiet_mode: print("skipping pair:", pixel1, pixel2, "diff =", diff, "possible vals =", pixel_pair_possible_vals, "(not in [0..255])")
-                continue
-
-            # find capacity and get msg bits
-            #################################
-
-            capacity = get_embedding_capacity(diff)
-
-            bits_value = format(diff_range.index(diff), '0'+str(capacity)+'b')
-
-            if debug_mode and not quiet_mode: print(f"[dbg] coords are {pixel_coords_1}, {pixel_coords_2}")
-            if debug_mode and not quiet_mode: print(f"[dbg] extracted bits {bits_value}, pixel values: {pixel1}, {pixel2}, diff is {diff}, range is {diff_range}")
-
-            bits = bits + bits_value
-
-    bits = bits[num_bits_for_size:] # remove size of message before returning
-    return bits
-
-def embed_data_into_image(cover_image, message_bits, output_filename):
+# embed data #
+##############
+# this is the main logic for embedding data
+def embed_data_into_image(cover_image, message_bits, output_filename, key=None):
     output_file = cover_image.copy()
     msg_index = 0
     msg_length = len(message_bits)
@@ -243,7 +215,7 @@ def embed_data_into_image(cover_image, message_bits, output_filename):
 
     old_msg_index = 0
 
-    for pixel_coords_1, pixel_coords_2 in pixel_pairs(cover_image):
+    for pixel_coords_1, pixel_coords_2 in pixel_pairs(cover_image, key=key):
         pixel1=cover_image.getpixel(pixel_coords_1)
         pixel2=cover_image.getpixel(pixel_coords_2)
         if msg_index >= msg_length:
@@ -258,9 +230,9 @@ def embed_data_into_image(cover_image, message_bits, output_filename):
             # find difference range for pixel pair
             # and also check if valid pixel pair
             #######################################
-            diff = calculate_gray_difference(pixel1, pixel2)
+            diff = calculate_difference(pixel1, pixel2)
             diff_range = find_difference_range((pixel1, pixel2))
-            pixel_pair_possible_vals = grayscale_new_vals((pixel1, pixel2), diff_range[-1], diff)
+            pixel_pair_possible_vals = calculate_new_vals_from_difference((pixel1, pixel2), diff_range[-1], diff)
             # from the paper. determine if pixelpair should be skipped:
             if pixel_pair_possible_vals[0] < 0 or pixel_pair_possible_vals[0] > 255 or pixel_pair_possible_vals[1] < 0 or pixel_pair_possible_vals[1] > 255:
                 # invalid pair
@@ -288,7 +260,7 @@ def embed_data_into_image(cover_image, message_bits, output_filename):
             # find new difference / pixel vals
             ###################################
             new_diff = diff_range[bits_value]
-            new_vals.append(grayscale_new_vals((pixel1, pixel2), new_diff, diff))
+            new_vals.append(calculate_new_vals_from_difference((pixel1, pixel2), new_diff, diff))
             if debug_mode and not quiet_mode: print(f"[dbg] -> changing difference from {diff} to {new_diff}. New vals are {new_vals[-1][0]} and {new_vals[-1][1]}")
 
 
@@ -310,6 +282,65 @@ def embed_data_into_image(cover_image, message_bits, output_filename):
     output_file.save(output_filename, "bmp")
     if not quiet_mode: print(f"Output image saved as {output_filename}")
 
+# extract data from stego image #
+#################################
+# this is the main logic for extraction
+
+def extract_data(steg_image, key=None):
+    bits = ""
+    bytes_to_read = 0 # will change later
+
+    for pixel_coords_1, pixel_coords_2 in pixel_pairs(steg_image, key=key):
+        # break when we've read all the message bits #
+        ##############################################
+        if len(bits) > num_bits_for_size and not bytes_to_read: # wait to determine msg size until we've read enough bits
+            bytes_to_read = int(bits[0:num_bits_for_size],2)
+        if bytes_to_read and len(bits) >= (8*bytes_to_read + num_bits_for_size): #
+            break
+
+        if debug_mode and not quiet_mode: print(f"[dbg] in extract_data(), color_mode is {color_mode}")
+        if color_mode == "grayscale":
+            pixel_components = [[steg_image.getpixel(pixel_coords_1),steg_image.getpixel(pixel_coords_2)]]
+        elif color_mode == "color":
+            pixel_components = [[steg_image.getpixel(pixel_coords_1)[0],steg_image.getpixel(pixel_coords_2)[0]], [steg_image.getpixel(pixel_coords_1)[1],steg_image.getpixel(pixel_coords_2)[1]], [steg_image.getpixel(pixel_coords_1)[2],steg_image.getpixel(pixel_coords_2)[2]]]
+        else:
+            print("ERROR: color mode unrecognized:", color_mode)
+            exit()
+        
+        for pixel1, pixel2 in pixel_components:
+            # find difference range for pixel pair
+            # and also check if valid pixel pair
+            #######################################
+            diff = calculate_difference(pixel1, pixel2)
+            diff_range = find_difference_range((pixel1, pixel2))
+            pixel_pair_possible_vals = calculate_new_vals_from_difference((pixel1, pixel2), diff_range[-1], diff)
+            # from the paper. determine if pixelpair should be skipped:
+            if pixel_pair_possible_vals[0] < 0 or pixel_pair_possible_vals[0] > 255 or pixel_pair_possible_vals[1] < 0 or pixel_pair_possible_vals[1] > 255:
+                # invalid pair
+                if not quiet_mode: print("skipping pair:", pixel1, pixel2, "diff =", diff, "possible vals =", pixel_pair_possible_vals, "(not in [0..255])")
+                continue
+
+            # find capacity and get msg bits
+            #################################
+
+            capacity = get_embedding_capacity(diff)
+
+            bits_value = format(diff_range.index(diff), '0'+str(capacity)+'b')
+
+            if debug_mode and not quiet_mode: print(f"[dbg] coords are {pixel_coords_1}, {pixel_coords_2}")
+            if debug_mode and not quiet_mode: print(f"[dbg] extracted bits {bits_value}, pixel values: {pixel1}, {pixel2}, diff is {diff}, range is {diff_range}")
+
+            bits = bits + bits_value
+
+    bits = bits[num_bits_for_size:] # remove size of message before returning
+    return bits
+
+
+# Add filesize to data #
+########################
+# this function will prepend a 32-bit
+# number to the data, which contains
+# the size of the data to extract
 def add_filesize_bits(bitstring):
     if debug_mode and not quiet_mode: print(f"Bitstring: {bitstring}")
 
@@ -334,33 +365,31 @@ def add_filesize_bits(bitstring):
 
     return combined_data
 
+# read message file #
+#####################
+# this will read in a message file and
+# convert the data to binary to be written
+def read_message_file(filepath):
+    with open(filepath, 'r') as file:
+        message = file.read()
+    # Convert message to binary string
+    return ''.join(format(ord(char), '08b') for char in message)
 
+# main function #
+#################
 def main():
-    global color_mode
-    global pixel_pair_mode
     args = init_commandline_args()
     validate_args(args) # will exit if invalid args
+    apply_settings_from_args(args)
 
-    if args.verbose:
-        global debug_mode
-        debug_mode = 1
-    if args.quiet:
-        global quiet_mode
-        quiet_mode = 1
 
-    if args.vertical:
-        pixel_pair_mode = "vertical"
-    if args.horizontal:
-        pixel_pair_mode = "horizontal"
-
-    if args.rgb:
-        color_mode = "color"
-    elif args.gray:
-        color_mode = "grayscale"
+    key=None
+    if args.key:
+        key = args.key
 
     if args.extract_image:
         steg_image = bitmap(args.extract_image)
-        data = extract_data(steg_image.image)
+        data = extract_data(steg_image.image, key=key)
         if debug_mode and not quiet_mode: print("your data is: ", data)
 
         if not args.output_file: # default to stdout if no output file given
@@ -382,14 +411,14 @@ def main():
     elif args.dry_run:
         cover_image = bitmap(args.cover_image)
         total_capacity = 0
-        for pixel_pair_coords in pixel_pairs(cover_image.image):
+        for pixel_pair_coords in pixel_pairs(cover_image.image, key=key):
             pixel_pair = [cover_image.image.getpixel(pixel_pair_coords[z]) for z in [0,1]]
             if color_mode == "grayscale":
                 total_capacity += get_embedding_capacity(pixel_pair[0] - pixel_pair[1])
-            else:
-                print("Error: color mode not implemented yet")
-                # TODO: implement color for testing purposes
-                diff = calculate_rgb_difference(pixel_pair[0], pixel_pair[1])
+            elif color_mode == "color":
+                total_capacity += get_embedding_capacity(pixel_pair[0][0] - pixel_pair[0][1])
+                total_capacity += get_embedding_capacity(pixel_pair[1][0] - pixel_pair[1][1])
+                total_capacity += get_embedding_capacity(pixel_pair[2][0] - pixel_pair[2][1])
         print("total capacity is:", total_capacity, "bits =", total_capacity // 8, "bytes =", total_capacity // 64, "MB")
 
     else:
@@ -399,7 +428,7 @@ def main():
         cover_image = bitmap(args.cover_image)
         message_bits = read_message_file(args.message_file)
         full_bits = add_filesize_bits(message_bits)
-        embed_data_into_image(cover_image.image, full_bits, args.output_file)
+        embed_data_into_image(cover_image.image, full_bits, args.output_file, key=key)
         if not quiet_mode: print(f"Data embedded successfully into {args.output_file}")
 
 
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # 2024_CS-4463-steganography_semester-project
+
+## Overview
+Pixel Value Differencing steganography project, using "A steganographic method for images by pixel-value differencing" by Da-Chun Wua and Wen-Hsiang Tsai (2002) as a guide
+
+## Installation
+
+```bash
+python3 -m pip install -r requirements.txt
+```
+ - Currently the only requirement is Pillow 10.4 for reading / writing the image file
+
+## Usage
+
+embed data into image:
+```bash
+python3 PVD.py -c ./test-images/lena_gray.bmp -m ./test-messages/small-msg.txt -o PVD_encoded-image.bmp
+```
+
+extract data from image:
+```bash
+python3 PVD.py -e PVD_encoded-image.bmp
+```
+
+hide data using randomized key, vertical pixel pairings, and color components:
+```bash
+python3 PVD.py -c ./test-images/lena.bmp --rgb -m ./test-messages/small-msg.txt -o PVD_encoded-image.bmp --key "mysecretkey" --vertical
+```
